299f0a4bfd5e38d78834d197386db72fdebd4506
GrabbitGames <154015851+GrabbitGames@users.noreply.github.com>
2024-03-08 20:13:15 -0500

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -133,62 +133,62 @@ function setCloak() { // applies only to premade cloaks
   switch (cloak) {
     case "search": // Google Search
       setTitle("Google");
-      setFavicon("/assets/img/favicons/google.ico");
+      setFavicon("/favi/google.ico");
       location.reload();
       break;
     case "drive": // Google Drive
       setTitle("My Dive - Google Drive");
-      setFavicon("/assets/img/favicons/google-drive-ico.ico");
+      setFavicon("/favi/google-drive-ico.ico");
       location.reload();
       break;
     case "classroom": // Google Classroom
       setTitle("Google Classroom");
-      setFavicon("/assets/img/favicons/google-classroom.ico");
+      setFavicon("/favi/google-classroom.ico");
       location.reload();
       break;
     case "gmail": // Gmail
       setTitle("Gmail");
-      setFavicon("/assets/img/favicons/gmail.ico");
+      setFavicon("/favi/gmail.ico");
       location.reload();
       break;
     case "word": // Word
       setTitle("Document.docx");
-      setFavicon("/assets/img/favicons/word.ico");
+      setFavicon("/favi/word.ico");
       location.reload();
       break;
     case "powerpoint": // Powerpoint
       setTitle("Presentation.pptx");
-      setFavicon("/assets/img/favicons/powerpoint.ico");
+      setFavicon("/favi/powerpoint.ico");
       location.reload();
       break;
     case "youtube": // YouTube 
       setTitle("YouTube");
-      setFavicon("/assets/img/favicons/youtube.ico");
+      setFavicon("/favi/youtube.ico");
       location.reload();
       break;
     case "calendar": // Google Calendar
       setTitle("Google Calendar");
-      setFavicon("/assets/img/favicons/google-calender.ico");
+      setFavicon("/favi/google-calender.ico");
       location.reload();
       break;
     case "meets": // Google Meet
       setTitle("Google Meet");
-      setFavicon("/assets/img/favicons/google-meet.ico");
+      setFavicon("/favi/google-meet.ico");
       location.reload();
       break;
     case "canvas": // Canvas 
       setTitle("Canvas");
-      setFavicon("/assets/img/favicons/canvas.ico");
+      setFavicon("/favi/canvas.ico");
       location.reload();
       break;
     case "zoom": // Zoom
       setTitle("Zoom");
-      setFavicon("/assets/img/favicons/zoom.ico");
+      setFavicon("/favi/zoom.ico");
       location.reload();
       break;
     case "khan": // Khan Academy
       setTitle("Khan Academy"); 
-      setFavicon("/assets/img/favicons/khan-academy.ico");
+      setFavicon("/favi/khan-academy.ico");
       location.reload();
       break;
   }
